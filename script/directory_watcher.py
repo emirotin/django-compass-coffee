2d5bc1a49a0ff44daedbba84fbf9d96859eae84e
Eugene Mirotin <emirotin@gmail.com>
2010-12-29 12:37:03 +0200

    Moved watcher to separate module, minot optimiztions

---
+++ script/directory_watcher.py
@@ -0,0 +1,72 @@
+# The script to monitor directory changes and execute specific files on changed files
+# Tasks are executed one by one, on keyboard interrupt the last task is waited to finish (not terminated)
+# written by Eugene Mirotin, December 2010
+# heavily based on http://www.bryceboe.com/2010/08/26/python-multiprocessing-and-keyboardinterrupt/
+
+import sys
+import os
+import multiprocessing, Queue
+from signal import signal, SIGINT, SIG_IGN
+
+os_walk = os.walk
+pjoin = os.path.join
+os_stat = os.stat
+    
+class DirectoryWatcher(object):
+    
+    def __init__(self, start_dir, mask=None):
+        self.start_dir = start_dir
+        self.mask = mask
+
+    def __enum_files(self):
+        for (path, dirs, files) in os_walk(self.start_dir):
+            for file in files:
+                if self.mask is not None and not self.mask.match(file):
+                    continue
+                yield pjoin(path, file)
+                
+    @staticmethod
+    def __process_file(job_queue, process):
+        signal(SIGINT, SIG_IGN)
+        if job_queue.empty():
+            return
+        try:
+            file = job_queue.get(block=False)
+            process(file)
+        except Queue.Empty:
+            pass
+    
+    def watch(self, process):
+        print 'Watching {0}'.format(self.start_dir)
+        filestamps = {}
+        
+        job_queue = multiprocessing.Queue()
+        current_worker = None
+        try:
+            while True:
+                workers = []
+                for file_name in self.__enum_files():
+                    try:
+                        mtime = os_stat(file_name).st_mtime
+                    except OSError:
+                        continue
+                    if file_name not in filestamps or mtime > filestamps[file_name]:
+                        filestamps[file_name] = mtime            
+                        job_queue.put(file_name)
+                        worker = multiprocessing.Process(target=DirectoryWatcher.__process_file,
+                                          args=(job_queue, process))
+                        workers.append(worker)
+                for worker in workers:
+                    current_worker = worker
+                    worker.start()
+                    worker.join()
+                current_worker = None
+                
+        except KeyboardInterrupt:
+            if current_worker:
+                print 'Received ctrl-c, waiting for running task to complete'
+                current_worker.join()
+                print '   ...done.'
+            else:
+                print 'Received ctrl-c, exiting'
+            return
--- script/haml_watch.py
+++ script/haml_watch.py
@@ -1,86 +1,29 @@
-# written by Eugene Mirotin
-# heavily based on http://www.bryceboe.com/2010/08/26/python-multiprocessing-and-keyboardinterrupt/
-
-import sys
-import time
+from directory_watcher import DirectoryWatcher
 import os
-import re
+import time
 
-import multiprocessing, signal, Queue
-
-SLEEP_INTERVAL = 5
-
-os_walk = os.walk
 pjoin = os.path.join
-re_haml = re.compile('.*\.haml$')
-compile_line = "ruby haml.rb {0} {1}"
+splitext = os.path.splitext
+relpath = os.path.relpath
 
-ROOT_DIR = os.path.realpath(pjoin(os.path.dirname(os.path.realpath(__file__)), '..'))
-ASSETS_DIR = os.path.realpath(pjoin(ROOT_DIR, 'src', 'django_compass_coffee', 'assets'))
-MEDIA_DIR = os.path.realpath(pjoin(ROOT_DIR, 'src', 'django_compass_coffee', 'media'))
+class HamlCompiler(object):
+    def __init__(self, source_dir, dest_dir):
+        self.source_dir = source_dir
+        self.dest_dir = dest_dir
 
-HAML_DIR = pjoin(ASSETS_DIR, 'haml')
-TEMPLATES_DIR = os.path.realpath(pjoin(ROOT_DIR, 'src', 'django_compass_coffee', 'templates'))
-
-def compile(source_file):
-    dest_file = pjoin(TEMPLATES_DIR, os.path.relpath(source_file, HAML_DIR))
-    dest_file = os.path.splitext(dest_file)[0] + '.html'
-    print 'Recompiling {0} to {1}'.format(source_file, dest_file)
-    print '   ...done.'
-
-def compile_next(job_queue):
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
-    if job_queue.empty():
-        return
-    try:
-        job = job_queue.get(block=False)
-        compile(job)
-    except Queue.Empty:
-        pass
-
-def enum_files(start_dir, mask):
-    for (path, dirs, files) in os_walk(start_dir):
-        for file in files:
-            if not mask.match(file):
-                continue
-            yield pjoin(path, file)
-
-def main():
-    print 'Watching {0}'.format(HAML_DIR)
-    filestamps = {}
-
-    os_stat = os.stat
-    
-    job_queue = multiprocessing.Queue()
-    current_worker = None
-    try:
-        while True:
-            workers = []
-            for file_name in enum_files(HAML_DIR, re_haml):
-                try:
-                    mtime = os_stat(file_name).st_mtime
-                except OSError:
-                    continue
-                if file_name not in filestamps or mtime > filestamps[file_name]:
-                    filestamps[file_name] = mtime            
-                    job_queue.put(file_name)
-                    worker = multiprocessing.Process(target=compile_next,
-                                      args=(job_queue,))
-                    workers.append(worker)
-            for worker in workers:
-                current_worker = worker
-                worker.start()
-                worker.join()
-            current_worker = None
-            
-            #time.sleep(SLEEP_INTERVAL)
-                
-    except KeyboardInterrupt:
-        print 'Received ctrl-c, waiting for running task to complete'
-        if current_worker:
-            current_worker.join()
+    compile_line = "ruby haml.rb {0} {1}"
+    def process(self, source_file):
+        dest_file = pjoin(self.dest_dir, relpath(source_file, self.source_dir))
+        dest_file = splitext(dest_file)[0] + '.html'
+        print 'Recompiling {0} to {1}'.format(source_file, dest_file)
+        time.sleep(10)
         print '   ...done.'
-        sys.exit(1)
+
  
 if __name__ == "__main__":
-    main()
+    import re
+    import sys
+    re_haml = re.compile('.*\.haml$')
+    processor = HamlCompiler(sys.argv[1], sys.argv[2])
+    watcher = DirectoryWatcher(sys.argv[1], re_haml)
+    watcher.watch(processor.process)
