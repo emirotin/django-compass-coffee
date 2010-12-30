27b6c48d3ac21327a72c6b12a88d6beb46e340b2
Eugene Mirotin <emirotin@gmail.com>
2010-12-30 19:12:20 +0200

    Fixes in utils - creating directory for templates / js before compiling

--- script/utils.py
+++ script/utils.py
@@ -39,12 +39,12 @@ def extensions_re(exts):
 
 def compile_coffee(source_file, source_dir, dest_dir):
     dest_file = pjoin(dest_dir, relpath(source_file, source_dir))
-    dest_file = dirname(dest_file)
+    dest_dir = dirname(dest_file)
     try:
-        os.makedirs(dest_file)
+        os.makedirs(dest_dir)
     except OSError:
         pass
-    print 'Recompiling {0} to {1}'.format(source_file, dest_file)        
+    print 'Compiling {0} to {1}'.format(source_file, dest_file)        
     retcode = call(["coffee",  "-o", dest_file, "-c", source_file], stderr=STDOUT)        
     print '   ...done. Return code: {0}'.format(retcode)
     return retcode
@@ -52,6 +52,11 @@ def compile_coffee(source_file, source_dir, dest_dir):
 def compile_haml(source_file, source_dir, dest_dir):
     dest_file = pjoin(dest_dir, relpath(source_file, source_dir))
     dest_file = splitext(dest_file)[0] + '.html'
-    print 'Recompiling {0} to {1}'.format(source_file, dest_file)
+    dest_dir = dirname(dest_file)
+    try:
+        os.makedirs(dest_dir)
+    except OSError:
+        pass
+    print 'Compiling {0} to {1}'.format(source_file, dest_file)
     retcode = call(["ruby", "haml.rb", source_file, dest_file], stderr=STDOUT)        
     print '   ...done. Return code: {0}'.format(retcode)
