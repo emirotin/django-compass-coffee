2c9e20cfcdcc3f6319b7b94cf50080db095407d3
Eugene Mirotin <emirotin@gmail.com>
2010-12-30 02:15:35 +0200

    make assets have the same structure as media / templates

--- script/build_assets.py
+++ script/build_assets.py
@@ -10,7 +10,7 @@ for folder in [
     clear_folder(folder)
     
 # build coffee files
-source_dir = '../src/django_compass_coffee/assets/coffee/'
+source_dir = '../src/django_compass_coffee/assets/media/js/'
 dest_dir = '../src/django_compass_coffee/media/js/'
 re_coffee = re.compile('.*\.coffee$')
 print 'Compiling coffee files to {0}'.format(dest_dir) 
@@ -19,7 +19,7 @@ for file in enum_files(source_dir, re_coffee):
     
 # build scss / sass with compass
 print 'Compiling stylesheets with compass'
-call(shlex.split("compass compile -c compass-config.rb"), cwd="../src/django_compass_coffee/assets/", stderr=STDOUT)
+call(shlex.split("compass compile -c compass-config.rb"), cwd="../src/django_compass_coffee/assets/media/", stderr=STDOUT)
 
     
 # copy static files
@@ -29,15 +29,20 @@ for folder, exts in (
         ('img', 'gif|png|jpg|jpeg')
     ):
     re_exts = extensions_re(exts)
-    source_dir = '../src/django_compass_coffee/assets/' + folder
+    source_dir = '../src/django_compass_coffee/assets/media/' + folder
     print 'Copying files from {0}'.format(source_dir)
     dest_dir = '../src/django_compass_coffee/media/' + folder
     for file in enum_files(source_dir, re_exts):
         print '    Copied {0}'.format(copy_file(file, source_dir, dest_dir))
+
+# check js files with jslint
+for file in enum_files('../src/django_compass_coffee/media/js'):
+    print "Running {0} through Javascript Lint".format(file)
+    call(shlex.split("../tools/jsl -conf ../tools/jsl.conf -nologo -process {0}".format(file)), stderr=STDOUT)
         
 # build haml files
 re_haml = re.compile('.*\.haml')
-source_dir = '../src/django_compass_coffee/assets/haml/'
+source_dir = '../src/django_compass_coffee/assets/templates/'
 dest_dir = '../src/django_compass_coffee/templates/'
 print 'Compiling haml files to {0}'.format(dest_dir)
 for file in enum_files(source_dir, re_haml):
@@ -46,7 +51,7 @@ for file in enum_files(source_dir, re_haml):
 
 # copy html files   
 re_html =  re.compile('.*\.html')
-source_dir = '../src/django_compass_coffee/assets/html/'
+source_dir = '../src/django_compass_coffee/assets/templates/'
 dest_dir = '../src/django_compass_coffee/templates/'
 print 'Copying html files to {0}'.format(dest_dir)
 for file in enum_files(source_dir, re_html):
--- script/run_dev.py
+++ script/run_dev.py
@@ -1,14 +1,14 @@
 commands = [
-    "python ./coffee_watch.py ../src/django_compass_coffee/assets/coffee ../src/django_compass_coffee/media/js", # compile coffee scripts
-    "python ./files_watch.py ../src/django_compass_coffee/assets/js ../src/django_compass_coffee/media/js js", # copy js libraries
+    "python ./coffee_watch.py ../src/django_compass_coffee/assets/media/js ../src/django_compass_coffee/media/js", # compile coffee scripts
+    "python ./files_watch.py ../src/django_compass_coffee/assets/media/js ../src/django_compass_coffee/media/js js", # copy js libraries
 
-    "python ./haml_watch.py ../src/django_compass_coffee/assets/haml ../src/django_compass_coffee/templates", # compile haml templates
-    "python ./files_watch.py ../src/django_compass_coffee/assets/html ../src/django_compass_coffee/media/html html", # copy regular html
+    "python ./haml_watch.py ../src/django_compass_coffee/assets/templates ../src/django_compass_coffee/templates", # compile haml templates
+    "python ./files_watch.py ../src/django_compass_coffee/assets/templates ../src/django_compass_coffee/templates html", # copy regular html
 
-    ("compass watch -c compass-config.rb", "../src/django_compass_coffee/assets/"), # run compass in watch mode to build sass
-    "python ./files_watch.py ../src/django_compass_coffee/assets/css ../src/django_compass_coffee/media/css css", # copy plain css
+    ("compass watch -c compass-config.rb", "../src/django_compass_coffee/assets/media/"), # run compass in watch mode to build sass
+    "python ./files_watch.py ../src/django_compass_coffee/assets/media/css ../src/django_compass_coffee/media/css css", # copy plain css
 
-    "python ./files_watch.py ../src/django_compass_coffee/assets/img ../src/django_compass_coffee/media/img \"gif|jpg|jpeg|png\"", # copy images
+    "python ./files_watch.py ../src/django_compass_coffee/assets/media/img ../src/django_compass_coffee/media/img \"gif|jpg|jpeg|png\"", # copy images
     
 #    ("python manage.py runserver", "../src/django_compass_coffee") # run django server
 ]
